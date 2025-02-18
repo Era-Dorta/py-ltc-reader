e41e59aa236d8ca8273e93e7d00351eff7a479dc
Era Dorta <e.dorta@tudelft.nl>
2025-02-18 12:10:03 +0100

    Change main to read from wav file

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import ltc_reader
-ltc_reader.start_read_ltc()
+from pathlib import Path
+
+def main(wav_file_path: Path) -> None:
+    with wav_file_path.open("rb") as f:
+        raw_data = f.read()
+
+    ltc_reader.decode_ltc(raw_data)
+
+if __name__ == "__main__":
+    main(Path("/home/era/Music/fireface-test/audio-data/01-241205_1405.wav"))
